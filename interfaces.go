b2fb985dc51fb55a4e9518737960c37d94357dea
MaxHenger <max_henger@hotmail.com>
2015-08-03 23:21:51 +0200

    Unified encryption/signing and decryption/verifying

--- interfaces.go
+++ interfaces.go
@@ -2,14 +2,8 @@ package nbnet
 
 type Encrypter interface {
 	Encrypt(message []byte) (encrypted []byte, err error)
-	Update() error
 }
 
 type Decrypter interface {
 	Decrypt(message []byte) (decrypted []byte, err error)
-}
-
-type Signer interface {
-	Sign(data []byte) ([]byte, error)
-	Verify(encrypted, message []byte) (ok bool, err error)
 }
